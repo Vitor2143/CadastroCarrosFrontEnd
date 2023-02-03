fcc1e5ba13aa15f7e8bd15bcf87fadb08276f415
@vitoraugustobbarros <vitoraugustobbarros@gmail.com>
2023-02-02 23:17:13 -0300

    alteração no update

--- script.js
+++ script.js
@@ -1,84 +1,75 @@
 const url = 'http://localhost:3000';
-const modal = document.querySelector('.modal-container')
-const sDeleteModal = document.querySelector('.deleteModalContainer')
-const tbody = document.querySelector('tbody')
-const sNomeCar = document.querySelector('#m-nomeCar')
-const sCodConc = document.querySelector('#m-codConcess')
-const sValorCar = document.querySelector('#m-valorCar')
-const sMarketPlace = document.querySelector('#marketPlace')
-const sSite = document.querySelector('#site')
-const sAnuncio = document.querySelector('#anuncio')
-const formModal = document.querySelector('#formModal')
+const modal = $('.modal-container')
+const sDeleteModal = $('.deleteModalContainer')
+const tbody = $('tbody')
+const formModal = $('#formModal')
 
 let itemIdGlobal = null
 let dadosdaAPI
-let itens
-let id
 
 function openModal() {
+    $('.modal-container').addClass("active");
 
-    modal.classList.add('active')
-
-    modal.onclick = e => {
-        if (e.target.className.indexOf('modal-container') !== -1) {
-            modal.classList.remove('active')
+    $('.modal-container').click(e => {
+        if (e.target.className.indexOf("modal-container") !== -1) {
+            $(".modal-container").removeClass("active");
             itemIdGlobal = null
-            console.log(itemIdGlobal)
+            location.reload()
         }
-    }
+    })
 }
 
 function deleteModal() {
-    sDeleteModal.classList.add('active')
-    sDeleteModal.onclick = e => {
-        if (e.target.className.indexOf('deleteModalContainer') !== -1) {
-            sDeleteModal.classList.remove('active')
-            itemIdGlobal = null
-            window.location.reload()
-
-
+    $('.deleteModalContainer').addClass("active");
+    $(".deleteModalContainer").click(e => {
+        if (e.target.className.indexOf("deleteModalContainer") !== -1) {
+            $(".deleteModalContainer").removeClass("active");
+            itemIdGlobal = null;
+            location.reload();
         }
-    }
+    })
 }
-
 
 function deleteConfirm(itemId) {
     if (itemIdGlobal !== itemId) {
         itemIdGlobal = itemId;
         deleteModal()
-        const btnDelete = document.querySelector('#btnDelete')
-        btnDelete.addEventListener('click', async () => {
+        let btnDelete = $('#btnDelete')
+        btnDelete.click(async () => {
             deleteItem(itemIdGlobal)
-            window.location.reload()
+            location.reload()
         }
         );
     }
 }
 
-function modalUpdate(itemId) {
+
+
+
+function modalUpdate(itemId, item) {
     openModal()
     modificaModalUpdate()
-    const btnUpdate = document.querySelector('#btnUpdate')
-    btnUpdate.addEventListener('click', () => {
+    const btnUpdate = $('#btnUpdate')
+    btnUpdate.click(async () => {
         try {
-            let codConc = document.querySelector('#m-codConcess').value
-            let nomeCar = document.querySelector('#m-nomeCar').value
-            let valorCar = parseInt(document.querySelector('#m-valorCar').value)
-            let marketPlace = document.querySelector('#marketPlace').checked
-            let site = document.querySelector('#site').checked
-            let anuncio = document.querySelector('#anuncio').checked
+            let codConc = $('#m-codConcess').val()
+            let nomeCar = $('#m-nomeCar').val()
+            let valorCar = parseInt($('#m-valorCar').val())
+            let marketPlace = $('#marketPlace').prop('checked')
+            let site = $('#site').prop('checked')
+            let anuncio = $('#anuncio').prop('checked')
 
             let dados = {
-                codigoConcess: codConc,
-                nomeCarro: nomeCar,
-                valorCarro: valorCar,
-                marketPlace: marketPlace,
-                site: site,
-                anuncio: anuncio,
+                codigoConcess: codConc || item.codigoConcess,
+                nomeCarro: nomeCar ||item.nomeCarro,
+                valorCarro: valorCar || item.valorCarro,
+                marketPlace: marketPlace || false,
+                site: site ||  false,
+                anuncio: anuncio || false
             };
 
             updateItem(dados, itemId);
-            window.location.reload()
+            location.reload()
         }
         catch (Error) {
             console.log(Error);
@@ -90,15 +81,15 @@ function modalUpdate(itemId) {
 function modalCadastro() {
     openModal()
     modificaModalCadastro()
-    const btnSalvar = document.querySelector('#btnSalvar')
-    btnSalvar.addEventListener('click', async () => {
+    let btnSalvar = $('#btnSalvar')
+    btnSalvar.click(async () => {
         try {
-            let codConc = document.querySelector('#m-codConcess').value
-            let nomeCar = document.querySelector('#m-nomeCar').value
-            let valorCar = parseInt(document.querySelector('#m-valorCar').value)
-            let marketPlace = document.querySelector('#marketPlace').checked
-            let site = document.querySelector('#site').checked
-            let anuncio = document.querySelector('#anuncio').checked
+            let codConc = $('#m-codConcess').val()
+            let nomeCar = $('#m-nomeCar').val()
+            let valorCar = parseInt($('#m-valorCar').val())
+            let marketPlace = $('#marketPlace').prop('checked')
+            let site = $('#site').prop('checked')
+            let anuncio = $('#anuncio').prop('checked')
 
             let carro = {
                 codigoConcess: codConc,
@@ -110,7 +101,7 @@ function modalCadastro() {
             };
 
             addNewItem(carro);
-            window.location.reload()
+            location.reload()
         } catch (Error) {
             console.log(Error.response.data);
             console.log(Error.response.status);
@@ -122,59 +113,43 @@ function modalCadastro() {
 
 function modificaModalCadastro() {
     retornaModalPadrao()
-    const modalTitle = document.querySelector('#modalTitle')
-    const modalButton = document.querySelector('.modalButton')
-    modalTitle.innerHTML = 'Cadastrar um carro'
-    modalButton.innerHTML = 'Salvar'
-    modalButton.setAttribute('id', 'btnSalvar')
-    modalButton.setAttribute('type', 'submit')
+    $('#modalTitle').html('Cadastrar um carro');
+    $('.modalButton').html('Salvar')
+        .attr('id', 'btnSalvar')
+        .attr('type', 'submit');
 }
 
 function modificaModalUpdate() {
     retornaModalPadrao()
-    const modalTitle = document.querySelector('#modalTitle')
-    const modalButton = document.querySelector('.modalButton')
-    modalTitle.innerHTML = 'Atualizar um carro'
-    modalButton.innerHTML = 'Atualizar'
-    modalButton.setAttribute('id', 'btnUpdate')
-    modalButton.setAttribute('type', 'submit')
+    $('#modalTitle').html('Atualizar um carro');
+    $('.modalButton').html('Atualizar')
+        .attr('id', 'btnUpdate')
+        .attr('type', 'submit');
 }
 
-function modificaModalDelete() {
-    const modalTitle = document.querySelector('#modalTitle')
-    const formModal = document.querySelector('#formModal')
-    const modalButton = document.querySelector('.modalButton')
-    const h4 = ('<h4>Tem Certeza?</h4>')
-    formModal.innerHTML = ''
-    modalTitle.innerHTML = h4
-    modalButton.innerHTML = 'Deletar'
-    modalButton.setAttribute('id', 'btnDelete')
-    modalButton.setAttribute('type', 'button')
-}
+
 
 function modificaModalPesquisa() {
-    const modalTitle = document.querySelector('#modalTitle')
-    const modalButton = document.querySelector('.modalButton')
-    modalTitle.innerHTML = 'Encontrar uma Concessionária'
-    modalButton.innerHTML = 'Encontrar'
-    modalButton.setAttribute('id', 'btnFind')
-    modalButton.setAttribute('type', 'submit')
-    formModal.innerHTML = (`<label for="m-codConcess">Código do Carro</label>
-    <input id="m-codConcess" type="text" />`)
+    $('#modalTitle').html('Encontrar uma Concessionária');
+    $('.modalButton').html('Encontrar')
+        .attr('id', 'btnFind')
+        .attr('type', 'submit');
+    $('#formModal').html(`<label for="m-codConcess">Código do Carro</label>
+        <input id="m-codConcess" type="text" />`)
 }
 
 function modalPesquisa() {
     openModal()
     modificaModalPesquisa()
-    let btnFind = document.querySelector('#btnFind')
-    btnFind.addEventListener('click', async () => {
+    let btnFind = $('#btnFind')
+    btnFind.click(async function () {
         try {
-            let codConc = document.querySelector('#m-codConcess').value
+            let codConc = $('#m-codConcess').val()
             const dados = {
                 codigoConcess: codConc
             }
+            $('.modal-container').removeClass('active')
             getOneItem(dados)
-            modal.classList.remove('active')
 
         } catch (error) {
             console.log(error)
@@ -183,52 +158,57 @@ function modalPesquisa() {
 }
 
 function retornaModalPadrao() {
-    formModal.innerHTML = (`<label for="m-codConcess">Código do Carro</label>
-    <input id="m-codConcess" type="text" />
-
-    <label for="m-nomeCar">Nome do Carro</label>
-    <input id="m-nomeCar" type="text" />
-
-    <label for="m-valorCar">Valor do Carro</label>
-    <input id="m-valorCar" type="number" />
-
-    <div class="plataformaCheck">
-      <div>
-        <label for="marketPlace">MarketPlace</label>
-        <input type="checkbox" id="marketPlace" class="plataformaInput" name="marketPlace" >
-      </div>
-      <div>
-        <label for="site">Site</label>
-        <input type="checkbox" id="site" class="plataformaInput" name="site" >
-      </div>
-      <div>
-        <label for="anuncio">Anuncio</label>
-        <input type="checkbox" id="anuncio" class="plataformaInput" name="anuncio">
-      </div>
-    </div>`)
+    $('#formModal').html(`<label for="m-codConcess">Código do Carro</label>
+        <input id="m-codConcess" type="text" />
+    
+        <label for="m-nomeCar">Nome do Carro</label>
+        <input id="m-nomeCar" type="text" />
+    
+        <label for="m-valorCar">Valor do Carro</label>
+        <input id="m-valorCar" type="number" />
+    
+        <div class="plataformaCheck">
+          <div>
+            <label for="marketPlace">MarketPlace</label>
+            <input type="checkbox" id="marketPlace" class="plataformaInput" name="marketPlace" >
+          </div>
+          <div>
+            <label for="site">Site</label>
+            <input type="checkbox" id="site" class="plataformaInput" name="site" >
+          </div>
+          <div>
+            <label for="anuncio">Anuncio</label>
+            <input type="checkbox" id="anuncio" class="plataformaInput" name="anuncio">
+          </div>
+        </div>`)
 }
 
 //FUNCAO CRIA TABELA
 function insertItem(item, itemId) {
 
     try {
-        let tr = document.createElement('tr')
+        let tr = $('<tr></tr>');
+        tr.html(`    
+                <td> ${item.codigoConcess} </td>
+                <td> ${item.nomeCarro} </td>
+                <td> ${item.valorCarro} </td>
+                <td> ${item.marketPlace ? `<i class="uil uil-check icon"></i>` : `<i class="uil uil-times icon"></i>`} </td>
+                <td> ${item.site ? `<i class="uil uil-check icon"></i>` : `<i class="uil uil-times icon"></i>`} </td>
+                <td> ${item.anuncio ? `<i class="uil uil-check icon"></i>` : `<i class="uil uil-times icon"></i>`} </td>
+                <td class="acao">
+                <button><i class="bx bx-edit"></i></button>
+                </td>
+                <td class="acao">
+                <button><i class="bx bx-trash"></i></button>
+                </td>`);
+        tr.find('button:first').on('click', function () {
+            modalUpdate(itemId, item);
+        });
 
-        tr.innerHTML = `    
-            <td> ${item.codigoConcess} </td>
-            <td> ${item.nomeCarro} </td>
-            <td> ${item.valorCarro} </td>
-            <td> ${item.marketPlace ? "Sim" : "Não"} </td>
-            <td> ${item.site ? "Sim" : "Não"} </td>
-            <td> ${item.anuncio ? "Sim" : "Não"} </td>
-            <td class="acao">
-            <button onclick = "modalUpdate(${itemId})"><i class="bx bx-edit"></i></button>
-            </td>
-            <td class="acao">
-            <button onclick="deleteConfirm(${itemId})"><i class="bx bx-trash"></i></button>
-            </td>`
-
-        tbody.appendChild(tr)
+        tr.find('button:last').on('click', function () {
+            deleteConfirm(itemId);
+        });
+        $('tbody').append(tr);
     } catch (erro) {
         alert(erro)
     }
@@ -238,12 +218,12 @@ function insertItem(item, itemId) {
 //FUNCAO CARREGA ITENS
 function loadItens() {
     let data
-    axios.get('http://localhost:3000/lista')
+    axios.get(`${url}/lista`)
         .then((response) => {
             dadosdaAPI = response.data;
             data = response.data;
             index = -1
-            tbody.innerHTML = ''
+            $('tbody').html('')
             for (let item of data) {
                 let itemId
                 index += 1
@@ -323,7 +303,8 @@ const getOneItem = async (dados) => {
             dadosdaAPI = response.data;
             data = response.data;
             index = -1
-            tbody.innerHTML = ''
+            $('tbody').html('')
+            $
             for (let item of data) {
                 index += 1
                 itemId = (dadosdaAPI[index].id)
@@ -340,6 +321,4 @@ const getOneItem = async (dados) => {
             }
         });
 };
-
-
 loadItens()
